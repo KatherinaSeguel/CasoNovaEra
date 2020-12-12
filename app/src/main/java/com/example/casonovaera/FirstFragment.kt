1926e27a8cd8b31a690609dd447072f30d6fca5b
Katherina <katherinaseguel@gmail.com>
2020-12-12 13:45:43 -0300

    Entity,Dao y Database listos API y Retrofit Listo Repositorio listo viewModel Listo adapter y primer fragmento

--- app/src/main/java/com/example/casonovaera/FirstFragment.kt
+++ app/src/main/java/com/example/casonovaera/FirstFragment.kt
@@ -6,26 +6,75 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.casonovaera.model.local.MaestraEntity
+import com.example.casonovaera.model.ui.productoAdapter
+import com.example.casonovaera.model.ui.productosViewModel
+import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(), productoAdapter.PasstheData {
 
+    //1) Declaro variable del View Model
+    lateinit var mViewModel: productosViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {  // 2.0  se crea este método
+        super.onCreate(savedInstanceState)
+        //2.1) asigno el View Model a esta vista
+        mViewModel= ViewModelProvider(this).get(productosViewModel::class.java)
+
+    }
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {  //aquí va el reyclerView
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        val mRecyclerView = idrecycler
+        val madapter=productoAdapter(this)
+        mRecyclerView.adapter=madapter
+        mRecyclerView.setHasFixedSize(true) //para que tengan el mismo tamaño
+        mRecyclerView.layoutManager= LinearLayoutManager(context) //si fueran una grilla sería GridLayout
+
+
+
+        //3) observo la finción que retorna del LiveData desde ViewModel
+        mViewModel.exposeLiveDataFromServer().observe(viewLifecycleOwner, Observer {
+            // Log.d("View",it.toString())
+            madapter.updateFrutos(it)
+
+        })
+
+
+        //   findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+
     }
+
+    override fun passTheData(mFrut: MaestraEntity) {
+
+        val mBundle=Bundle()
+
+        mBundle.putString("id",mFrut.image)   //put debe ser int o string según corresponda
+
+
+        //llega el registro a editar
+        Toast.makeText(context,mFrut.name, Toast.LENGTH_LONG).show()
+
+        //estoy pasando el objeto Bundle, de un fragmento a otro
+        findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment,mBundle)
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/casonovaera/model/ui/productoAdapter.kt
@@ -0,0 +1,79 @@
+package com.example.casonovaera.model.ui
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.casonovaera.R
+import com.example.casonovaera.model.local.MaestraEntity
+import kotlinx.android.synthetic.main.primer.view.*
+
+class productoAdapter  (var mPasstheData:PasstheData):RecyclerView.Adapter<productoAdapter.ProductoViewHolder>() { //paso 4 ,Implementar Recycler View
+
+        private  var mData= emptyList<MaestraEntity>()  //paso 1
+
+        //cada vez que haya un cambio actualiza la lista
+
+        fun updateFrutos(mStringList: List<MaestraEntity>) {  //paso 2
+
+            mData = mStringList        //paso 2
+            notifyDataSetChanged()
+        }
+
+        inner class ProductoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), View.OnClickListener{  //paso 3
+            // val itemView = itemView.setOnClickListener(this) cuando es imagen
+
+            var mitemView = itemView.id  //texview que está en ItemLIst
+            var imagen= itemView.imageproducto
+            var nombreUno= itemView.nombrepro
+            val precioUno= itemView.preciopro
+
+            val itemView = itemView.setOnClickListener(this)  // Cuando se hace click en el texto
+
+
+            override fun onClick(p0: View?) {    //paso 5
+                mPasstheData.passTheData(mData[adapterPosition])
+            }
+
+
+
+        }
+
+
+        //esto se genera después del paso 4, son los métodos que se generan
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductoViewHolder {//paso 4.1
+            //creas el viewholder, trabaja en el xml,se crea el XML
+            val itemView= LayoutInflater.from(parent.context).inflate(R.layout.primer,parent,false)
+
+            return ProductoViewHolder(itemView)
+
+        }
+
+
+        override fun onBindViewHolder(holder: ProductoViewHolder, position: Int) {//paso 4.2
+            //llena el xml con los objetos del listado, une los datos con los elementos
+
+            val mfrut= mData[position]
+            holder.mitemView= mfrut.id
+            holder.nombreUno.text=mfrut.name
+           // holder.precioUno= mfrut.price
+
+        }
+
+        override fun getItemCount(): Int {   // paso 4.3
+            //este tienen el total de elementos
+            return mData.size
+        }
+
+
+        //esta interface se declara y se ocupa en el primer fragmanto .
+        interface PasstheData{
+
+            fun passTheData(mFrut:MaestraEntity)
+        }
+
+
+
+
+    }
